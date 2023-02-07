38b51968ff281016218513d3055704e52b8b01fd
bph-dev <bphillips.swe@gmail.com>
2023-02-06 19:26:41 -0500

    fix: add comments, unset any local variables.

--- symlink.sh
+++ symlink.sh
@@ -4,6 +4,7 @@
 # 0 - OK, or destination exists
 # 1 - Incorrect usage
 
+# condition: check the number of positional params by expanding them
 if [ "$#" -ne "2" ]; then
     echo "Usage: symlink.sh from to" >&2
     exit 1
@@ -12,6 +13,7 @@ fi
 SOURCE="$1"
 DEST="$2"
 
+# condition: check if the file/dir is present 
 if [ -e "$DEST" ]; then
     if [ "$(readlink "$DEST")" = "$SOURCE" ]; then
         # Already linked; destination exists; ignore
@@ -24,7 +26,11 @@ fi
 
 # Make $DEST's directory if it doesn't exist
 DEST_DIR="$(dirname "$DEST")"
-[ "$DEST_DIR" != "" ] && mkdir -p -- "$DEST_DIR"
+# condition: check if the destination dir is not empty, then proceed if true
+[ "$DEST_DIR" != "" ] && mkdir -p -- "$DEST_DIR" 
 
 # Link!
 ln -s -- "$SOURCE" "$DEST"
+
+unset SOURCE DEST DEST_DIR
+
