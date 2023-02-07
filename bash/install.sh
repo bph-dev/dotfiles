0a822157b635f9aabb7a5c475e1404168c9e867c
bph-dev <bphillips.swe@gmail.com>
2023-02-06 19:23:45 -0500

    feat: add condition to check for the existence of a bashrc_d folder in the home directory and run all files within it.

--- bash/install.sh
+++ bash/install.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/sh
 
-"$SYM_LINK" "$LOCAL_PROJ/bash_aliases" "$HOME/.bash_aliases"
+cat >> "$HOME/.bashrc" <<EOF
+# User specific aliases and functions
+if [ -d ~/.bashrc.d ]; then
+  for rc in ~/.bashrc.d/*; do
+    if [ -f "\$rc" ]; then
+      . "\$rc"
+    fi
+  done
+fi
+unset rc
+
+# kubectl completion
+#source <(kubectl completion bash | sed s/kubectl/k/g)
+EOF
+
+"$SYM_LINK" "$LOCAL_PROJ/bashrc.d/bash_aliases" "$HOME/.bashrc.d/bash_aliases"
+
